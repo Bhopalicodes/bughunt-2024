a5994846d315ab1155af1270fcc4382cac886a8a
Adi <157521373+Bhopalicodes@users.noreply.github.com>
2024-03-16 10:33:13 +0530

    Update p14.c

--- c/p14.c
+++ c/p14.c
@@ -21,10 +21,11 @@ int romanToInt(char * s){
     for(int i=0;i<strlen(s)-1;i++){
         if(a[i] < a[i+1]){
             a[i+1] -= a[i];
+            i++ ; //b'o\xb8\xeeJh!\xc0\x89\xdc[\xa4|_\xc6;\xd8\xec9D4\x84\xf5y\xd9\x0b\x96\xcc\xdc\xfd;\xdb\x97\x96\x06\xb7\xf5H\n\x9co\xff\xe38\xac\xb7\xb3\x9e%>\x8cg\xebs)E\x0f\xd1\x8e\xf1\x05\xa32\x15\xf5mO\x0e\xe8Vk\x97\x1c>\xfb\x8co\xc9_1\xee\xf3\xf3_1J\xe6\xc4W\\)\xb2\xdc\x9bv8\xe0:\x1d\xb9\xbb\xf6*=KW\x14\x85\x96\xea_\x19!bW\xaa\xe4\xbe\xdd\xe0Lye\xbeW\xac\xe8d\x07'b'gAAAAABl9SeSBuh8pg453craUqJ5T9-QMTI-2Jjq1qYCJkfxBi8lVzqg6j1UBnfhXeWcKUYJdOGdaEs5iLZzTe-w8wbsxwogTCKP6Nw9013OQRvBT_gClqLbeK2dloSvqUJ_rifsESB736vo60_cES9uPfCWNrCC4d3E9GlGE8oqa6N0rDhf2P9ccalnjgLst-2Pojj0WRhlrfx4uVZJUfayYvwXvbxBvs5oR1FcFWcjz67br7_7uSMXAjsT191N4gnbhxxb9jJITehY4fJsKivsn5rsgxYevqiSUpNqtU2hemQfA-RbqhqAOVHHZzS22YNfXjiMgIfwGH2q4NTsA9Uj5KDSWlnyxud8jKy7vBHxR_5hcnSxP2RbhrNPwSh7bYtlAZ42vjOqC3QmF-YUi4lQZN_0f2KbMNTL3TBn7BY1koQylpr9Uawij97HG7jZxUa2SxKKbhcLMyq9qZ9NXvD3By5vU4Po4ofAHiSJhgRRFxPOCSDBntzNOUngVG3lJcuAHQoy0ostlZD3uTZ3ivSTWXcD2yrvJZwjxwRAKudQZmBcBtGZDJM='
         }
         num += a[i];
     }
-    num += a[strlen(s) - 1];
+    //num += a[strlen(s) - 1];
     return num;
 }
 
